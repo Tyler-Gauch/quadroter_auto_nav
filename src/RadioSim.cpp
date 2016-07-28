e1103f7bdfb780bf988987cecb66fbc1fefea31a
Tyler Gauch <tyler.gauch@continuum.net>
2016-07-28 19:01:03 -0400

    Uses slam_out_pose now and we get a decent reading

--- src/RadioSim.cpp
+++ src/RadioSim.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <cmath>
+
 #include <ros/ros.h>
 //ROS Topic Headers
 #include <geometry_msgs/Twist.h>
@@ -10,53 +12,95 @@
 //TF Headers
 #include <tf/transform_listener.h>
 
+#define STABILIZE_SAMPLE_THRESHOLD 10
 
-geometry_msgs::Twist lastCommand;
+ros::NodeHandle * nh;
 
-void cmdVelCallback(const geometry_msgs::Twist &msg)
+ros::Subscriber cmd_vel_sub;	//gets the wanted cmd_vel tranforms
+ros::Subscriber pose_sub;	//gets the wanted cmd_vel tranforms
+
+float changeY = 0, changeX = 0;
+float wantedHoldX = 0, wantedHoldY = 0;
+bool firstTime = true;
+bool mapStabilized = false;
+float currentXTotal = 0, currentYTotal = 0;
+int numSamples = 0;
+
+void cmdVelCallback(const geometry_msgs::Twist &lastCommand)
 {
-	lastCommand = msg;
+	
+}
+
+void poseCallback(const geometry_msgs::PoseStamped &currentPosition){
+	
+	float currentX = currentPosition.pose.position.x * 100; //into cm
+	float currentY = currentPosition.pose.position.y * 100; //into cm
+
+	if(firstTime)
+	{
+		firstTime = false;
+		wantedHoldX = currentX;
+		wantedHoldY = currentY;
+		return;
+	}
+
+	changeX = currentX - wantedHoldX;
+	changeY = currentY - wantedHoldY;
+
+	if(abs(changeX) < 0.5f)
+	{
+		changeX = 0;
+	}
+
+	if(abs(changeY) < 0.5f)
+	{
+		changeY = 0;
+	}
+
+	//lets keep getting scans until we have a decent map
+	if(!mapStabilized)
+	{
+		numSamples++;
+		currentXTotal += changeX;
+		currentYTotal += changeY;
+
+		if(numSamples > STABILIZE_SAMPLE_THRESHOLD)
+		{
+			float avgX = currentXTotal / numSamples;
+			float avgY = currentYTotal / numSamples;			
+
+			if(avgX > 1 || avgY > 1)
+			{
+				numSamples = 0;
+				currentXTotal = 0;
+				currentYTotal = 0;
+				std::cout << "Waiting for map to stabilize..." << std::endl;
+				return;
+			}else{
+				mapStabilized = true;
+				std::cout << "Map stabilized" << std::endl;
+			}
+		}else{
+			return;
+		}
+	}
+
+	std::cout << "DX\t" << changeX << "\tDY:\t" << changeY << std::endl;
+
 }
 
 int main(int argc, char** argv){
 
 	ros::init(argc, argv, "quadroter_auto_nav_radio_sim");
-	ros::NodeHandle nh;
 
-	ros::Subscriber cmd_vel_sub;	//gets the wanted cmd_vel tranforms
-	tf::TransformListener listener;	//listens for location updates
-	tf::StampedTransform transform;	//holds our last known location
+	nh = new ros::NodeHandle();
 
-	cmd_vel_sub = nh.subscribe("/cmd_vel", 1, cmdVelCallback); //topic, queue size, callback
+	cmd_vel_sub = nh->subscribe("/cmd_vel", 1, cmdVelCallback); //topic, queue size, callback
+	pose_sub = nh->subscribe("/slam_out_pose", 1, poseCallback); //topic, queue size, callback
 
-	float changeY = 0, changeX = 0;
-	float lastX = 0, lastY = 0;
-
-	while(nh.ok())
+	while(nh->ok())
 	{
 		ros::spinOnce(); // needed to get subscribed messages
-
-		try
-		{
-			//gets the current transform from the map to the base_link
-			listener.lookupTransform("/map", "/base_link", ros::Time(0), transform);
-			//gets us our current x,y coordinate in the occupancy grid
-			float currentX = transform.getOrigin().x();
-			float currentY = transform.getOrigin().y();
-			
-			changeY = currentY - lastY;
-			changeX = currentX - lastX;
-
-			std::cout << "X/DX" << currentX <<"/" << changeX << " Y/DY " << currentY << "/" << changeY << std::endl;
-
-			lastX = currentX;
-			lastY = currentY;
-			
-		}catch(tf::TransformException &ex)
-		{
-			ROS_ERROR("%s", ex.what());
-			continue;
-		}
 	}
 
 }
